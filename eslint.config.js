f2f212cd6d85908888dd06b2d6e2dba6f2d7b818
engrjeff <engineerjeff15@gmail.com>
2026-07-08 18:31:41 +0800

    chore: setup

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+//  @ts-check
+
+import { tanstackConfig } from "@tanstack/eslint-config"
+
+export default [
+  ...tanstackConfig,
+  {
+    rules: {
+      "import/no-cycle": "off",
+      "import/order": "on",
+      "sort-imports": "on",
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/require-await": "off",
+      "pnpm/json-enforce-catalog": "off",
+    },
+  },
+  {
+    ignores: ["eslint.config.js", ".prettierrc", "src/components/ui/**"],
+  },
+]
